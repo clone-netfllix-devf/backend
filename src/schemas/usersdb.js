c513c3a144fa9413651411edcc53b2512af96c9a
LigorioSalgado <salgadolg5@hotmail.com>
2018-05-31 18:33:35 -0700

    hasheando passwords

--- src/schemas/usersdb.js
+++ src/schemas/usersdb.js
@@ -1,4 +1,7 @@
 var _mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
 
 var Schema = _mongoose.default.Schema;
 
@@ -20,15 +23,14 @@ var UserSchema = new Schema({
   'isPremium': {
 
         type: Boolean,
-
-        require: true
+        default:false,
+        require: false
 
     },
   'address': {
 
         type: String,
-
-        require: true
+        require: false
 
     },
   'ccHash': {
@@ -63,9 +65,41 @@ var UserSchema = new Schema({
 
         type: Date,
 
-        require: true
+        require: false
 
     },
   },{'collection': 'user', timestamps: true });
+
+  UserSchema.pre('save',(next) => {
+    let user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      if (err) return next(err);
+  
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+  
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+  });
+  
+  
+
+
+  })
+
+
+
+  UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 exports.default = _mongoose.default.model('user', UserSchema);
