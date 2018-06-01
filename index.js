d29b6169c4dd7166a9421565b3d20623d4d8acbf
LigorioSalgado <salgadolg5@hotmail.com>
2018-05-31 18:07:49 -0700

    primer tipo de graphql

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ var mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000
 
+mongoose.connect('mongodb://netflix:netflix2018@ds139970.mlab.com:39970/clone-netflix');
+const db = mongoose.connection;
+
+db.on('error',() => console.log("Failed to conect to database"))
+    .once('open', () => console.log("Connected to the database"))
+
+
+
+
 app.get('/', (req,res) => {
     res.send("Hello World");
 })
